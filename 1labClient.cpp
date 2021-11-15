3d8a0903b224aa301383356d6d99908cef8de907
TimaSL <tima.strelkov2288@gmail.com>
2021-11-15 21:27:50 +0300

    first commit

---
+++ 1labClient.cpp
@@ -0,0 +1,264 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include "Source_h.h"
+#include <locale.h>
+#include <conio.h>
+
+#pragma warning(disable:4996)
+#pragma comment (lib, "Rpcrt4.lib")
+
+#define SIZE 100
+
+using namespace std;
+
+void download_to_server(int index_client)
+{
+	char PathToFile[SIZE] = { 0 }, FileName[64] = { 0 };
+	int DataOfFile[cMaxBuf] = { 0 };
+	unsigned int len_buf = 0, len_path = 0, len_FileName = 0;
+	int res, EndOfFile = 0, symbol;
+
+	cout << "Введите путь до файла, который будет загружен на сервер: ";
+	cin >> PathToFile;
+	len_path = strlen(PathToFile);
+
+	int i;
+	for (i = len_path - 1; PathToFile[i] != '\\'; i--);
+	for (i += 1; i < len_path && len_FileName < 63; i++)
+	{
+		FileName[len_FileName] = PathToFile[i];
+		len_FileName++;
+	}
+	char FileNameOnServer[SIZE] = { 0 };
+	cout << "Введите путь для загрузки файла на сервер: ";
+	cin >> FileNameOnServer;
+
+	FILE* file = fopen(PathToFile, "rb");
+	while (1)
+	{
+		while (len_buf < cMaxBuf)
+		{
+			res = fread(&symbol, 1, 1, file);
+			if (res == 0)
+			{
+				EndOfFile = 1;
+				break;
+			}
+			DataOfFile[len_buf++] = symbol;
+		}
+
+		res = MakeFileOnServer((const unsigned char*)FileNameOnServer, DataOfFile, len_buf, index_client, EndOfFile);
+
+		if (res < 0)
+		{
+			cout << "Ошибка авторизации." << endl;
+			break;
+		}
+		memset(DataOfFile, 0, cMaxBuf);
+		len_buf = 0;
+		if (EndOfFile)
+		{
+			cout << "Файл успешно закачен на сервер." << endl;
+			break;
+		}
+	}
+	if (file != NULL)
+		fclose(file);
+}
+
+void download_from_server(int index_client)
+{
+	char PathToFile[SIZE] = { 0 }, FileName[64] = { 0 };
+	int DataOfFile[cMaxBuf] = { 0 };
+	unsigned int len_buf = 0, len_path = 0, len_FileName = 0;
+	int res, EndOfFile = 0;
+	cout << "Введите путь до файла: ";
+	cin >> PathToFile;
+	for (; PathToFile[len_path] != L'\0'; len_path++);
+
+	int i = 0;
+	for (i = len_path - 1; PathToFile[i] != '\\'; i--);
+	for (i += 1; i < len_path && len_FileName < 63; i++)
+	{
+		FileName[len_FileName] = PathToFile[i];
+		len_FileName++;
+	}
+	FileName[len_FileName] = '\0';
+
+	bool IsOpenedFile = true;
+	FILE* file = NULL;
+	while (1)
+	{
+
+		res = CopyOnClient((const unsigned char*)PathToFile, DataOfFile, &len_buf, index_client, &EndOfFile);
+		if (res < 0)
+		{
+			cout << "Нет доступа." << endl;
+			break;
+		}
+		if (IsOpenedFile)
+		{
+			file = fopen(FileName, "wb");
+			IsOpenedFile = false;
+		}
+		for (unsigned int i = 0; i < len_buf; i++)
+			fwrite(&DataOfFile[i], 1, 1, file);
+
+		memset(DataOfFile, 0, cMaxBuf);
+		if (EndOfFile)
+			break;
+	}
+	if (file != NULL)
+	{
+		cout << "Файл был успешно скачен." << endl;
+		fclose(file);
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	RPC_STATUS status;
+	RPC_CSTR szStringBinding = NULL;
+
+	char IPv4[32];
+	char ListeningPort[8];
+	cout << "Введите IPv4: ";
+	cin >> IPv4;
+	cout << "Введите порт: ";
+	cin >> ListeningPort;
+
+
+	//Функция RpcStringBindingCompose создает дескриптор привязки строки.
+	status = RpcStringBindingComposeA(
+		NULL,                                        // UUID to bind to.
+		(RPC_CSTR)("ncacn_ip_tcp"),					// Use TCP/IP protocol.
+		(RPC_CSTR)(IPv4),				// TCP/IP network address to use.
+		(RPC_CSTR)(ListeningPort),					// TCP/IP port to use.
+		NULL,										// Protocol dependent network options to use.
+		&szStringBinding);							// String binding output.
+
+	if (status)
+	{
+		cout << "Подключение отсутсвует." << endl;
+		exit(status);
+	}
+
+	//Функция RpcBindingFromStringBinding возвращает дескриптор привязки из строкового представления дескриптора привязки.
+	status = RpcBindingFromStringBindingA(
+		szStringBinding,        // The string binding to validate.
+		&hExample1Binding);     // Put the result in the implicit binding
+								// handle defined in the IDL file.
+	if (status)
+		exit(status);
+
+	//Номер клиента
+	int index_client = 0, size_pass;
+	char login[SIZE] = { 0 };
+	char password[SIZE] = { 0 };
+	cout << "Введите логин: ";
+	cin >> login;
+	cout << "Введите пароль: ";
+	for (size_pass = 0; size_pass < SIZE && password[size_pass - 1] != L'\r'; size_pass++)
+	{
+		password[size_pass] = _getch();
+	}
+	password[size_pass - 1] = '\0';
+	system("cls");
+	cout << "Ожидание входа..." << endl;
+
+	int res = MakeClientOnServer((const unsigned char*)login, (const unsigned char*)password, &index_client);
+	printf("Присвоен индекс %d\n", index_client);
+	switch (res)
+	{
+	case -2:
+		cout << "Превышено кол-во клиентов.";
+		_getch();
+		return 0;
+		break;
+	case -1:
+		cout << "Ошибка авторизации.";
+		_getch();
+		return 0;
+		break;
+	}
+	cout << "Вход успешно выполнен." << endl;
+
+	RpcTryExcept
+	{
+		char PathToFile[SIZE] = { 0 };
+		int ChoosenAction = 0;
+		while (1)
+		{
+			cout << "1) Скачать файл с сервера" << endl << "2) Закачать файл на сервер" << endl << "3) Удалить файл с сервера" << endl << "4) Выход" << endl << "Введите номер действия: ";
+			cin >> ChoosenAction;
+			switch (ChoosenAction)
+			{
+			case 1:
+				download_from_server(index_client);
+				break;
+			case 2:
+				download_to_server(index_client);
+				break;
+			case 3:
+				cout << "Введите путь до файла, который будет удалён: ";
+				cin >> PathToFile;
+				res = DeleteFileOnServer((const unsigned char*)PathToFile, index_client);
+				if (res < 0)
+					cout << "Ошибка." << endl;
+				else
+					cout << "Файл успешно удалён." << endl;
+				break;
+
+			case 4:
+				ClientOut(index_client);
+				goto OUT_;
+				break;
+			default:
+				cout << "Введена некорректная команда." << endl;
+			}
+			memset(PathToFile, 0, SIZE);
+			ChoosenAction = 0;
+		}
+	}
+		RpcExcept(1)
+	{
+		std::cerr << "Runtime reported exception " << RpcExceptionCode()
+			<< std::endl;
+	}
+	RpcEndExcept
+
+		OUT_ :
+	// Free the memory allocated by a string.
+	status = RpcStringFreeA(
+		&szStringBinding); // String to be freed.
+
+	if (status)
+		exit(status);
+
+	// Releases binding handle resources and disconnects from the server.
+	status = RpcBindingFree(
+		&hExample1Binding); // Frees the implicit binding handle defined in the IDL file.
+
+	if (status)
+		exit(status);
+}
+
+// Memory allocation function for RPC.
+// The runtime uses these two functions for allocating/deallocating
+// enough memory to pass the string to the server.
+void* __RPC_USER midl_user_allocate(size_t size)
+{
+	return malloc(size);
+}
+
+// Memory deallocation function for RPC.
+void __RPC_USER midl_user_free(void* p)
+{
+	free(p);
+}
---
+++ 1labServer.cpp
@@ -0,0 +1,251 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//#include "pch.h"
+#include <iostream>
+#include "Source_h.h"
+#include <fstream>
+#include <windows.h>
+#include <stdio.h>
+using namespace std;
+
+#pragma warning(disable:4996)
+#pragma comment (lib, "Rpcrt4.lib")
+const unsigned int cMaxClient = 50;
+
+struct client
+{
+	handle_t client_handle;
+	FILE* file;
+	bool IsClosedFile;
+};
+
+client ALL_Clients[cMaxClient];
+unsigned int count_clients = 0;
+bool IsConnectedClient[cMaxClient] = { false };
+
+int MakeClientOnServer(
+	/* [in][string] */ const unsigned char* login,
+	/* [in][string] */ const unsigned char* password,
+	/* [out] */ int* index)
+{
+	if (count_clients >= cMaxClient)
+		return -2;
+	int new_index = 0;
+
+	for (; (IsConnectedClient[new_index]) && new_index < cMaxClient; new_index++);
+	if (new_index == cMaxClient)
+		return -2;
+	handle_t handle = 0;
+	//Если функция завершается успешно, вы получаете дескриптор токена, который представляет вошедшего в систему пользователя. \
+	Затем вы можете использовать этот дескриптор токена для олицетворения указанного пользователя или, в большинстве случаев, \
+	для создания процесса, который выполняется в контексте указанного пользователя.
+	if (!LogonUserA((LPCSTR)login, NULL, (LPCSTR)password, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, &handle))
+	{
+		cout << "Не получен дескриптор токена.";
+		return -1;
+	}
+	//Функция ImpersonateLoggedOnUser позволяет вызывающему потоку олицетворять контекст безопасности вошедшего в систему пользователя. \
+	Пользователь представлен дескриптором токена.
+	if (!ImpersonateLoggedOnUser(handle))
+	{
+		cout << "Ошибка имперсонизации.";
+		return -1;
+	}
+	ALL_Clients[new_index].client_handle = handle;
+	ALL_Clients[new_index].IsClosedFile = true;
+	ALL_Clients[new_index].file = NULL;
+	IsConnectedClient[new_index] = true;
+	count_clients++;
+	(*index) = new_index;
+	cout << "Клиент успешно подключился." << endl;
+	return 1;
+}
+
+void Output(
+	/* [string][in] */ const unsigned char* szOutput)
+{
+	std::cout << szOutput << std::endl;
+}
+
+
+
+int CopyOnClient(
+	/* [in][string] */ const unsigned char* path,
+	/* [out] */ int buf[65534],
+	/* [out] */ unsigned int* length_buf,
+	/* [in] */ int index,
+	/* [out] */ int* check_eof)
+{
+	if (!ImpersonateLoggedOnUser(ALL_Clients[index].client_handle))
+	{
+		cout << "У клиента №" << index << " ошибка имперсонизации." << endl;
+		return -1;
+	}
+	if (ALL_Clients[index].IsClosedFile)
+	{
+		cout << "Первое открытие файла!\n";
+		ALL_Clients[index].file = fopen((const char*)path, "rb");
+		ALL_Clients[index].IsClosedFile = false;
+	}
+	if (!ALL_Clients[index].file)
+	{
+		cout << "Файл закрыт";
+		cout << "Нет доступа." << endl;
+		ALL_Clients[index].IsClosedFile = true;
+		return -1;
+	}
+
+	int symbol = 0;
+	int len = 0;
+	while (len < cMaxBuf)
+	{
+		int check = fread(&symbol, 1, 1, ALL_Clients[index].file);
+		if (check == 0)
+		{
+			*check_eof = 1;
+			fclose(ALL_Clients[index].file);
+			ALL_Clients[index].IsClosedFile = true;
+			break;
+		}
+		buf[len] = symbol;
+		len++;
+	}
+	(*length_buf) = len;
+	return 1;
+}
+
+int MakeFileOnServer(
+	/* [in][string] */ const unsigned char* FileName,
+	/* [in] */ int buf[65534],
+	/* [in] */ int len_buf,
+	/* [in] */ int index,
+	/* [in] */ int EndOfFile)
+{
+
+	if (!ImpersonateLoggedOnUser(ALL_Clients[index].client_handle))
+	{
+		cout << "У клиента № ошибка имперсонизации." << endl;
+		return -1;
+	}
+
+	if (ALL_Clients[index].IsClosedFile)
+	{
+		cout << "Первое открытие файла!\n";
+		ALL_Clients[index].file = fopen((const char*)(FileName), "wb");
+		ALL_Clients[index].IsClosedFile = false;
+	}
+	if (!ALL_Clients[index].file)
+	{
+		cout << "Файл закрыт\n";
+		cout << "У клиента нет прав для данного действия" << endl;
+		ALL_Clients[index].IsClosedFile = true;
+		return -1;
+	}
+	for (unsigned int i = 0; i < len_buf; i++)
+	{
+		fwrite(&buf[i], 1, 1, ALL_Clients[index].file);
+	}
+
+	if (EndOfFile)
+	{
+		ALL_Clients[index].IsClosedFile = true;
+		fclose(ALL_Clients[index].file);
+	}
+
+	return 1;
+}
+
+int DeleteFileOnServer(
+	/* [in][string] */ const unsigned char* PathToFile,
+	/* [in] */ int index)
+{
+
+	if (!ImpersonateLoggedOnUser(ALL_Clients[index].client_handle))
+	{
+		cout << "У клиента ошибка имперсонизации." << endl;
+		return -1;
+	}
+
+	if (remove((const char*)PathToFile) == -1)
+	{
+		cout << "У клиента нет прав для данного действия" << endl;
+		return -1;
+	}
+	return 1;
+}
+
+int ClientOut(
+	/* [in] */ int index)
+{
+	if (index > cMaxClient || index < 0)
+		return -1;
+	IsConnectedClient[index] = false;
+	ALL_Clients[index].client_handle = 0;
+	ALL_Clients[index].IsClosedFile = true;
+	ALL_Clients[index].file = NULL;
+	count_clients--;
+	cout << "Клиент отключился." << endl;
+	return 1;
+}
+
+// Naive security callback.
+RPC_STATUS CALLBACK SecurityCallback(RPC_IF_HANDLE /*hInterface*/, void* /*pBindingHandle*/)
+{
+	return RPC_S_OK; // Always allow anyone.
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	RPC_STATUS status;
+	RpcServerRegisterAuthInfoA(nullptr, RPC_C_AUTHN_WINNT, 0, nullptr);
+	//Функция RpcServerUseProtseqEp сообщает библиотеке времени выполнения RPC использовать указанную \
+	последовательность протокола в сочетании с указанной конечной точкой для приема вызовов удаленных процедур.
+	status = RpcServerUseProtseqEpA(
+		(RPC_CSTR)("ncacn_ip_tcp"),			// Use TCP/IP protocol.
+		RPC_C_PROTSEQ_MAX_REQS_DEFAULT,		// Backlog queue length for TCP/IP.
+		(RPC_CSTR)("9000"),					// TCP/IP port to use.
+		NULL);								// No security.
+
+	if (status)
+		exit(status);
+
+	//Функция RpcServerRegisterIf2 регистрирует интерфейс с библиотекой времени выполнения RPC.
+	status = RpcServerRegisterIf2(
+		Example1_v1_0_s_ifspec,              // Interface to register.
+		NULL,                                // Use the MIDL generated entry-point vector.
+		NULL,                                // Use the MIDL generated entry-point vector.
+		RPC_IF_ALLOW_CALLBACKS_WITH_NO_AUTH, // Forces use of security callback.
+		RPC_C_LISTEN_MAX_CALLS_DEFAULT,      // Use default number of concurrent calls.
+		(unsigned)-1,                        // Infinite max size of incoming data blocks.
+		SecurityCallback);                   // Naive security callback.
+
+	if (status)
+		exit(status);
+	cout << "Сервер работает..." << endl;
+
+	// Функция RpcServerListen сигнализирует библиотеке времени выполнения RPC прослушивать удаленные вызовы процедур
+	status = RpcServerListen(
+		1,                                   // Recommended minimum number of threads.
+		RPC_C_LISTEN_MAX_CALLS_DEFAULT,      // Recommended maximum number of threads.
+		FALSE);                              // Start listening now.
+	cout << "Сервер выключен" << endl;
+	if (status)
+		exit(status);
+}
+
+// Memory allocation function for RPC.
+// The runtime uses these two functions for allocating/deallocating
+// enough memory to pass the string to the server.
+void* __RPC_USER midl_user_allocate(size_t size)
+{
+	return malloc(size);
+}
+
+// Memory deallocation function for RPC.
+void __RPC_USER midl_user_free(void* p)
+{
+	free(p);
+}
